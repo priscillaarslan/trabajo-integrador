52902b1853e0a8bf08965dacc71304fb411da7b7
Roccopellegrinoo <rpellegrino@udesa.edu.ar>
2022-06-14 13:48:24 -0300

    nuevo

---
+++ js/index.js
@@ -0,0 +1,106 @@
+// Canciones mas esuchadas
+
+let url = ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks');
+
+fetch(url)
+
+  .then(function (respuesta) {
+    return respuesta.json()
+  })
+  .then(function (datos) {
+    console.log(datos.data)
+    let artistas = datos.data
+
+    for (let i = 0; i <= 4; i++) {
+
+
+
+      document.querySelector('.section1').innerHTML += ` 
+<article>
+
+<img class="img1" src=${artistas[i].album.cover} alt="" >
+<a href="./detalledeartista.html?idCantante=${artistas[i].id}">
+<p class= "artistas3" > ${artistas[i].artist.name}</p>
+</a>
+<a href="./detallecanciones.html?idcancion=${artistas[i].id}">
+ <h3 class="nombre"> ${artistas[i].title}</h3>
+ </a>
+
+    
+
+</article>`
+
+
+    }
+
+  })
+
+  .catch(function (error) {
+    console.log(error);
+  })
+
+
+// Albumes mas escuchados
+let url2 = ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/albums');
+
+fetch(url2)
+
+  .then(function (respuesta) {
+    return respuesta.json();
+  })
+  .then(function (datos) {
+    console.log(datos.data)
+    let almbums = datos.data
+    for (let i = 0; i <= 4; i++) {
+
+      document.querySelector('.section2').innerHTML += `
+            <article>
+            <img class="img1" src=${almbums[i].cover}>
+            <a href="./detalledeldisco.html?Idalmbumes=${almbums[i].id}">
+              <p class="artistas3"> ${almbums[i].title}</p>
+            </a>
+            
+            
+            `
+
+
+    }
+  })
+
+
+  .catch(function (error) {
+    console.log(error);
+  })
+
+
+
+
+//Artistas mas esuchados 
+let url3 = ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists');
+
+fetch(url3)
+
+  .then(function (respuesta) {
+    return respuesta.json()
+  })
+  .then(function (datos) {
+    let artistas = datos.data
+    console.log(datos.data)
+    for (let i = 0; i <= 4; i++) {
+
+
+      document.querySelector('.section4').innerHTML += `
+      <article>
+      <img class="img1" src=${artistas[i].picture}>
+      <a href="./detalledeartista.html?Idartistas=${artistas[i].id}">
+        <h3 class="artistas3"> ${artistas[i].name}</h3>
+      </a>
+     
+      
+      
+      
+      
+      `
+
+    }
+  })
---
+++ js/resultadodebusqueda.js
@@ -0,0 +1,48 @@
+let results = document.querySelector('.section3');
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let busqueda = queryStringObj.get('q');
+console.log(busqueda)
+
+fetch ('https://api.allorigins.win/raw?url=https://api.deezer.com/search?q' + busqueda)
+
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (datos) {
+        let buscar = datos.data
+        console.log(datos);
+        console.log("BUSCAR",buscar.length)
+        
+        console.log(0<buscar.length)
+        if (buscar.length == 0){ 
+            results,innerHTML= `
+            <img scr="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"/>
+            `
+        }
+        if (buscar.length > 0){
+        for (let i = 0; i < buscar.length; i++) {
+            
+            results.innerHTML += `<article>
+            <img scr=${buscar[i].picture}        alt=""   />
+            <a href="./detallecanciones.html?Id=${buscar[i].id}"> 
+            <p>Name:${buscar[i].title}   </p>
+            </a>
+            </article>
+            `
+            }} else{
+                results.innerHTML=`
+                <h1 class="artistas3">No hay resultados</h1> 
+                `
+            } 
+
+           
+        
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+
+
+
