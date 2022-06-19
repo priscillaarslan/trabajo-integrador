c4a07017edf1494238e085cbc3d91cf616d52f84
Martina Mendez <martinamendez@Martinas-MacBook-Air-2.local>
2022-06-19 19:37:05 -0300

    agregue

---
+++ js/detalledeartista.js
@@ -0,0 +1,10 @@
+// paso 1, capturar id rn querystring 
+/* 
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let id = queryStringObj.get("id");
+console.log(id);
+*/
+// paso 2, hacert fetch a endpoint de artista con id
+// paso 3, capturaR seccion del html 
+// paso 4, ponerle el html a section con la info del endpoint
